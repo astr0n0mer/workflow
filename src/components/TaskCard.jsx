75205a29551170e07ec152d0a9a7c7ae654f1790
Imran Khan (MAQ LLC) <v-khanimr@microsoft.com>
2023-06-19 18:40:44 +0530

    Fix broken task assets links

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -6,8 +6,18 @@ import User3 from "../assets/users/user-3.png";
 import User4 from "../assets/users/user-4.png";
 import User5 from "../assets/users/user-5.png";
 import Message from "../assets/navigation-messages.svg";
+import Dog from "../assets/task-assets/dog.png";
+import Flower from "../assets/task-assets/flower.png";
+import MobileAppDesign from "../assets/task-assets/mobile-app-design.png";
+import Plant from "../assets/task-assets/plant.png";
 
 const avatars = [User1, User2, User3, User4, User5];
+const taskAssets = {
+  "dog.png": Dog,
+  "flower.png": Flower,
+  "mobile-app-design.png": MobileAppDesign,
+  "plant.png": Plant,
+};
 
 const priorityColors = {
   low: "bg-[#DFA87433] text-[#D58D49]",
@@ -41,11 +51,7 @@ export default function TaskCard({
       <div className="flex flex-wrap justify-between gap-3 2xl:flex-nowrap">
         {content.map((c, i) =>
           contentType === "image" ? (
-            <img
-              key={i}
-              src={`./src/assets/task-assets/${c}`}
-              className="flex-1"
-            />
+            <img key={i} src={taskAssets[c]} className="flex-1" />
           ) : (
             <p key={i} className="text-xs text-secondary">
               {c}
